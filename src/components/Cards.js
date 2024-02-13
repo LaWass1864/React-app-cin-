c1672207bf3784676e06ce2d70c05982bf4435d4
LaWass1864 <amwass4@hotmail.com>
2024-02-13 23:55:32 +0100

    formattage du genre ok

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,14 +1,106 @@
-import React from 'react';
+import React from "react";
 
-//  on destructure pour aller chercher chaque movie
-const Cards = ( movie ) => {
-
+const Card = ({ movie }) => {
+// format de la date de sortie
+    const dateFormater = (date) => {
+        let [yy, mm, dd] = date.split("-");
+        return [dd, mm, yy].join("/");
+    };
+// formattage du genre
+    const genreFinder = () => {
+        let genreArray = [];
+        for (let i = 0; i < movie.genre_ids.length; i++) {
+            switch (movie.genre_ids[i]) {
+                case 28:
+                    genreArray.push(`Action`);
+                    break;
+                case 12:
+                    genreArray.push(`Aventure`);
+                    break;
+                case 16:
+                    genreArray.push(`Animation`);
+                    break;
+                case 35:
+                    genreArray.push(`Comédie`);
+                    break;
+                case 80:
+                    genreArray.push(`Policier`);
+                    break;
+                case 99:
+                    genreArray.push(`Documentaire`);
+                    break;
+                case 18:
+                    genreArray.push(`Drame`);
+                    break;
+                case 10751:
+                    genreArray.push(`Famille`);
+                    break;
+                case 14:
+                    genreArray.push(`Fantasy`);
+                    break;
+                case 36:
+                    genreArray.push(`Histoire`);
+                    break;
+                case 27:
+                    genreArray.push(`Horreur`);
+                    break;
+                case 10402:
+                    genreArray.push(`Musique`);
+                    break;
+                case 9648:
+                    genreArray.push(`Mystère`);
+                    break;
+                case 10749:
+                    genreArray.push(`Romance`);
+                    break;
+                case 878:
+                    genreArray.push(`Science-fiction`);
+                    break;
+                case 10770:
+                    genreArray.push(`Téléfilm`);
+                    break;
+                case 53:
+                    genreArray.push(`Thriller`);
+                    break;
+                case 10752:
+                    genreArray.push(`Guerre`);
+                    break;
+                case 37:
+                    genreArray.push(`Western`);
+                    break;
+                default:
+                    break;
+            }
+        }
+        return genreArray;
+    };
 
     return (
+        <div className="card">
+            <img
+                src={
+                    movie.poster_path
+                        ? "https://image.tmdb.org/t/p/original/" + movie.poster_path
+                        : "./img/poster.jpg"
+                }
+                alt={`affiche ${movie.title}`}
+            />
+            <h2>{movie.title}</h2>
+            {movie.release_date ? (
+                <h5>Sorti le : {dateFormater(movie.release_date)}</h5>
+            ) : null}
+            <h4>
+                {movie.vote_average.toFixed(1)}/10 <span>⭐</span>
+            </h4>
+            {/* genre des films */}
+            <ul>
+                {movie.genre_ids
+                    ? genreFinder()
+                    : movie.genres.map((genre) => <li key={genre}>{genre.name}</li>)}
+            </ul>
 
-
-    )
-
+        </div>
+    );
 };
 
-export default Cards;
+export default Card;
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,18 +3,84 @@ import React, { Component, useEffect, useState } from 'react';
 import Cards from './Cards';
 
 const Form = () => {
-//  on met les data dans la constante moviesData et un useState de type array
+    //  on met les data dans la constante moviesData et un useState de type array
     const [moviesData, setMoviesData] = useState([])
     //  quand le composant est appelé, tu vas me faire la chose suivante
     useEffect(() => {
         axios.get(
-            `https://api.themoviedb.org/3/search/movie?api_key=c906cb5d4e8234c1ed739bdce117f432&query=war`
-            )
+            `https://api.themoviedb.org/3/search/movie?api_key=c906cb5d4e8234c1ed739bdce117f432&query=foot`
+        )
             //  on va rechercher le tableau depuis l'element results
-        .then((res) => setMoviesData(res.data.results));
-//  ensuite on se controle en allant dans la console > components > Form > Hooks > State 
-          }, []);
+            .then((res) => setMoviesData(res.data.results));
+        //  ensuite on se controle en allant dans la console > components > Form > Hooks > State
+    }, []);
 
+    constgenreFinder = () => {
+        let genreArray = [];
+        for (let index = 0; index < movie.genre_ids.length; index++) {
+            switch (movie.genre_ids[index]) {
+                case 28:
+                    genreArray.push(`Action`);
+                    break;
+                case 12:
+                    genreArray.push(`Aventure`);
+                    break;
+                case 16:
+                    genreArray.push(`Animation`);
+                    break;
+                case 35:
+                    genreArray.push(`Comédie`);
+                    break;
+                case 80:
+                    genreArray.push(`Policier`);
+                    break;
+                case 99:
+                    genreArray.push(`Documentaire`);
+                    break;
+                case 18:
+                    genreArray.push(`Drame`);
+                    break;
+                case 10751:
+                    genreArray.push(`Famille`);
+                    break;
+                case 14:
+                    genreArray.push(`Fantasy`);
+                    break;
+                case 36:
+                    genreArray.push(`Histoire`);
+                    break;
+                case 27:
+                    genreArray.push(`Horreur`);
+                    break;
+                case 10402:
+                    genreArray.push(`Musique`);
+                    break;
+                case 9648:
+                    genreArray.push(`Mystère`);
+                    break;
+                case 10749:
+                    genreArray.push(`Romance`);
+                    break;
+                case 878:
+                    genreArray.push(`Science-fiction`);
+                    break;
+                case 10770:
+                    genreArray.push(`Téléfilm`);
+                    break;
+                case 53:
+                    genreArray.push(`Thriller`);
+                    break;
+                case 10752:
+                    genreArray.push(`Guerre`);
+                    break;
+                case 37:
+                    genreArray.push(`Western`);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
     return (
         <div className='form-component'>
             <div className='form-container'>
@@ -42,14 +108,12 @@ const Form = () => {
             </div >
             {/* composant qui va stocker les resultats */}
             <div className='result'>
-
-                {moviesData.slice(0,12).map((movie) => (
+                {moviesData.slice(0, 12).map((movie) => (
                     <Cards movie={movie} key={movie.id} />
                 ))}
-            
             </div>
         </div >
     );
-    }
+}
 
 export default Form;
